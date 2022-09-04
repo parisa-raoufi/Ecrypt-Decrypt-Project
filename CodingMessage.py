f0a3d2fc738d44c2718770328ec1562254a8fb05
parisa-raoufi <90569583+parisa-raoufi@users.noreply.github.com>
2022-09-04 09:46:28 +0430

    refactor with pylama

--- CodingMessage.py
+++ CodingMessage.py
@@ -1,55 +1,42 @@
 
-Message =  input("Enter our message =>   ")
-Code=int (input("Enter code number that you want to use =>   "))
-Mode=int(input("Enter 1 if you want ecrypt the message and if you want to decrypt the message, enter 2 pls =>   "))
+Message = input("Enter our message =>   ")
+Code = int(input("Enter code number that you want to use =>   "))
+ContextMode = "Enter 1 if you want ecrypt the message and if you want to decrypt the message, enter 2 pls =>   "
+Mode = int(input(ContextMode))
 
 
-def ecrypting(message , code):
-    Length_Message = len(message)   
-    NewMessage=""
-    for letter in range(0 ,Length_Message) :
-      FirstUniCodeLetter= ord(message[letter])     
-      SeconderyUniCodeLetter= FirstUniCodeLetter + code
-      Newlatter=chr(SeconderyUniCodeLetter)
-      NewMessage=NewMessage+Newlatter
-      
+def ecrypting(message, code):
+    Length_Message = len(message)
+    NewMessage = ""
+    for letter in range(0, Length_Message):
+        FirstUniCodeLetter = ord(message[letter])
+        SeconderyUniCodeLetter = FirstUniCodeLetter + code
+        Newlatter = chr(SeconderyUniCodeLetter)
+        NewMessage = NewMessage + Newlatter
+
     print(NewMessage)
 
 
+def decrypting(message, code):
+    Length_Message = len(message)
+    NewMessage = ""
+    for letter in range(0, Length_Message):
+        FirstUniCodeLetter = ord(message[letter])
+        SeconderyUniCodeLetter = FirstUniCodeLetter - code
+        Newlatter = chr(SeconderyUniCodeLetter)
+        NewMessage = NewMessage + Newlatter
 
-def decrypting(message , code):
-    Length_Message = len(message)   
-    NewMessage=""
-    for letter in range(0 ,Length_Message) :
-      FirstUniCodeLetter= ord(message[letter])     
-      SeconderyUniCodeLetter= FirstUniCodeLetter - code
-      Newlatter=chr(SeconderyUniCodeLetter)
-      NewMessage=NewMessage + Newlatter
-      
     print(NewMessage)
 
-    
 
-def CodeMassage (message , code , mode):
-    if mode==1:
-        ecrypting(message , code)
-    elif mode==2:
-        decrypting(message , code)
+def CodeMassage(message, code, mode):
+    if mode == 1:
+        ecrypting(message, code)
+    elif mode == 2:
+        decrypting(message, code)
     else:
         mode = int(input("Pls just enter 1 or 0 => "))
-        CodeMassage(message , code , mode)
-    
+        CodeMassage(message, code, mode)
 
 
-
-CodeMassage(Message , Code , Mode)
-
-
-
-
-
-
-
-
-
-
+CodeMassage(Message, Code, Mode)
